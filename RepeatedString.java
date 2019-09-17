4165e2ca16d864b38e489edcfa98a6448b616236
Gérfesson Santos <gerfessonsn@gmail.com>
2019-09-16 23:50:41 -0300

    12/23 test cases failed
    Given a string and some number, return the times of the letter 'a' repeats in the number interval. The string will be repeated until has equal or upper dimension that number.

---
+++ RepeatedString.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        char[] sr = s.toCharArray();
+        long size = sr.length, sumremainder = 0, summultiply = 0, remainder = 0, multiply = 0, j = 0;
+        if(size > 0){
+            multiply = n/size;
+            remainder = n%size;
+        }
+        for(int i=0; i<size; i++){
+            if(sr[i] == 'a'){
+                if(j < remainder){
+                    sumremainder++;
+                }
+                summultiply++;
+                j++;
+            }
+        }
+        if(remainder > multiply){
+            return remainder;
+        } else {
+            return ((summultiply*multiply)+sumremainder);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
